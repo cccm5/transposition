fb30bf562178d2d398160eaf7b9170228f820691
dhd15101 <daniel.h.davidson@uconn.edu>
2020-10-29 14:57:03 -0400

    Initial commit

---
+++ src/main/java/dev/cccm5/transposition/Transposition.java
@@ -0,0 +1,17 @@
+package dev.cccm5.transposition;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Transposition extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
